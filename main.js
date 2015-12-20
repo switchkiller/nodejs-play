8e182e7247458e9318e655bb97db3ca770e44ddd
switchkiller <emailbarung@gmail.com>
2015-12-20 13:59:03 +0545

    Using text as input - callback concepts

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 var http = require("http");
-
+var fs = require('fs');
 http.createServer(function(require, response){
   response.writeHead(200, {'Content-type':'text/plain'});
-  response.end('Hey There\n');
+  fs.readFile('input.txt', function (err, data){
+    if (err) return console.error(err);
+    response.end(data.toString());
+  });
 }).listen(8081);
 
 console.log('Serving at 127.0.0.1:8081');
